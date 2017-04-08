4dd5a8791c8707251359aaf30bd377a66fe069e1
Mario Hennenberger <mario@hennenberger.de>
2017-04-08 19:41:20 -0400

    add config

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://babystartup.de/shop/admin/');
+define('HTTP_CATALOG', 'http://babystartup.de/shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://babystartup.de/shop/admin/');
+define('HTTPS_CATALOG', 'http://babystartup.de/shop/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/babystartup.de/opencart-babystartup/upload/admin/');
+define('DIR_SYSTEM', '/var/www/babystartup.de/opencart-babystartup/upload/system/');
+define('DIR_IMAGE', '/var/www/babystartup.de/opencart-babystartup/upload/image/');
+define('DIR_CATALOG', '/var/www/babystartup.de/opencart-babystartup/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_SYSTEM . 'storage/cache/');
+define('DIR_DOWNLOAD', DIR_SYSTEM . 'storage/download/');
+define('DIR_LOGS', DIR_SYSTEM . 'storage/logs/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'storage/modification/');
+define('DIR_UPLOAD', DIR_SYSTEM . 'storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'babystartup');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
